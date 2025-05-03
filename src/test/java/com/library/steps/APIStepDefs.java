5404c648c131ed04e24ae19331d89612b3bb072d
rajae.ha <rajae.hamida@gmail.com>
2025-05-03 03:15:25 -0400

    US02 Completed

--- src/test/java/com/library/steps/APIStepDefs.java
+++ src/test/java/com/library/steps/APIStepDefs.java
@@ -6,12 +6,16 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 
-import static org.hamcrest.Matchers.everyItem;
-import static org.hamcrest.Matchers.notNullValue;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
 
 public class APIStepDefs {
     // 1st future file ==>Retrieve all users from the API endpoint
@@ -19,10 +23,10 @@ public class APIStepDefs {
     RequestSpecification givenPart;
     Response response;
     ValidatableResponse thenPart;
+    JsonPath jp ;
 
     @Given("I logged Library api as a {string}")
     public void i_logged_library_api_as_a(String role) {
-
         givenPart = RestAssured.given().log().uri();
         givenPart.header("x-library-token", LibraryAPI_Util.getToken(role));
 
@@ -36,6 +40,7 @@ public class APIStepDefs {
     public void i_send_get_request_to_endpoint(String endpoint) {
         response = givenPart.when().get(endpoint);
         response.prettyPrint();
+        jp= response.jsonPath();
 
     }
     @Then("status code should be {int}")
@@ -53,8 +58,31 @@ public class APIStepDefs {
     @Then("Each {string} field should not be null")
     public void each_field_should_not_be_null(String path) {
         thenPart.body(path, everyItem(notNullValue()));
+    }
+    /**************************************** US02****************************/
+    String storedPathParamValue;
+
+    @Given("Path param {string} is {string}")
+    public void path_param_is(String pathParam, String pathParamValue) {
+        givenPart.pathParam(pathParam,pathParamValue);
+        storedPathParamValue = pathParamValue;
+    }
+
+    @Then("{string} field should be same with path param")
+    public void field_should_be_same_with_path_param(String pathParam) {
+        String actualValue = jp.getString(pathParam);
+        Assert.assertEquals("Field doesn't match path param", storedPathParamValue, actualValue);
 
     }
+
+    @Then("following fields should not be null")
+    public void following_fields_should_not_be_null(List<String> fields) {
+        for (String field : fields) {
+            Assert.assertNotNull("Expected '" + field + "' to be not null, but it was null",jp.get(field));
+        }
+
+    }
+
 
 
 
